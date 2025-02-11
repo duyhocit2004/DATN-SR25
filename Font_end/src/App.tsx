78b71cac097934da64cc2a790a0fcb9708742e45
Nguyễn Quốc Cường <cuonggau251125@gmail.com>
2025-02-12 01:12:28 +0700

    update login and register no call api /database

---
+++ Font_end/src/App.tsx
@@ -0,0 +1,29 @@
+import { Routes, Route, Link, Navigate } from "react-router-dom"
+import Header from "./component/Header";
+import React from "react";
+import Login from "./client/auth/login";
+import Register from "./client/auth/register";
+import HomePage from "./component/HomePage";
+import Product from "./component/Products";
+import ProductDetail from "./component/ProductDetail";
+import Footer from "./component/Footer";
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/products' element={<Product />} />
+        <Route path='/productdetail' element={<ProductDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
+
+export default App;
