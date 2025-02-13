92ba901e1433173ba0c45e40eeb0c47ed46cbbc6
Nam2808-git <nam28082004@gmail.com>
2025-02-13 21:44:16 +0700

    'Capnhat'

--- Back_end/routes/web.php
+++ Back_end/routes/web.php
@@ -25,29 +25,27 @@ Route::get('/', function () {
 });
 
 // Route sản phẩm và biến sản phẩm
-route::get('product',[ProductController::class,'index'])->name('product');
-route::get('createProduct',[ProductController::class,'create'])->name('createProduct');
-route::post('postProduct',[ProductController::class,'Store'])->name('postProduct');
-route::get('edit/{id}/Product',[ProductController::class,'edit'])->name('get.Product');
-route::put('update/{id}/Product',[ProductController::class,'update'])->name('update.Product');
-route::delete('delete/{id}/Product',[ProductController::class,'destroy'])->name('delete.Product');
+route::get('product', [ProductController::class, 'index'])->name('product');
+route::get('createProduct', [ProductController::class, 'create'])->name('createProduct');
+route::post('postProduct', [ProductController::class, 'Store'])->name('postProduct');
+route::get('edit/{id}/Product', [ProductController::class, 'edit'])->name('get.Product');
+route::put('update/{id}/Product', [ProductController::class, 'update'])->name('update.Product');
+route::delete('delete/{id}/Product', [ProductController::class, 'destroy'])->name('delete.Product');
 //softDelete product
-route::get('Listdelete/Product',[ProductController::class,'trashedProducts'])->name('ListDelete.Product');
-route::get('restoneDelete/{id}/Product',[ProductController::class,'restoreProduct'])->name('restoreProduct.Product');
-route::delete('forceDelete/{id}/Product',[ProductController::class,'forceDelete'])->name('forceDelete.Product');
+route::get('Listdelete/Product', [ProductController::class, 'trashedProducts'])->name('ListDelete.Product');
+route::get('restoneDelete/{id}/Product', [ProductController::class, 'restoreProduct'])->name('restoreProduct.Product');
+route::delete('forceDelete/{id}/Product', [ProductController::class, 'forceDelete'])->name('forceDelete.Product');
 
 
 // Route màu
-route::get('color',[colorController::class,'index'])->name("color");
-route::get('createcolor',[colorController::class,'create'])->name("createcolor");
-route::post('postcolor',[colorController::class,'store'])->name("postcolor");
+route::get('color', [colorController::class, 'index'])->name("color");
+route::get('createcolor', [colorController::class, 'create'])->name("createcolor");
+route::post('postcolor', [colorController::class, 'store'])->name("postcolor");
 Route::get('/color/{id}/edit', [ColorController::class, 'edit'])->name('getcolor');
-route::put('updatecolor/{id}/update',[colorController::class,'Update'])->name("updatecolor");
-route::delete('deletecolor/{id}/delete',[colorController::class,'destroy'])->name("deletecolor");
+route::put('updatecolor/{id}/update', [colorController::class, 'Update'])->name("updatecolor");
+route::delete('deletecolor/{id}/delete', [colorController::class, 'destroy'])->name("deletecolor");
 
-Route::prefix('admmin')->middleware('admin')->group(function(){
-
-});
+Route::prefix('admmin')->middleware('admin')->group(function () {});
 
 // Route size
 Route::get('sizes', [SizeController::class, 'index'])->name('sizes.index');
@@ -65,23 +63,27 @@ Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edi
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
 Route::delete('users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 
-Route::get('variant',[VariantController::class,'index'])->name('variant.index');
-Route::get('get\{id}\variant',[VariantController::class,'edit'])->name('variant.listid');
-Route::get('create\{id}\variant',[VariantController::class,'create'])->name('variant.create');
-Route::get('show\variant',[VariantController::class,'show'])->name('variant.show');
-Route::post('store\variant',[VariantController::class,'store'])->name('variant.store');
-Route::put('update\{id}\variant',[VariantController::class,'update'])->name('variant.update');
-Route::delete('delete\{id}\variant',[VariantController::class,'destroy'])->name('variant.delete');
+Route::get('variant', [VariantController::class, 'index'])->name('variant.index');
+Route::get('get\{id}\variant', [VariantController::class, 'edit'])->name('variant.listid');
+Route::get('create\{id}\variant', [VariantController::class, 'create'])->name('variant.create');
+Route::get('show\variant', [VariantController::class, 'show'])->name('variant.show');
+Route::post('store\variant', [VariantController::class, 'store'])->name('variant.store');
+Route::put('update\{id}\variant', [VariantController::class, 'update'])->name('variant.update');
+Route::delete('delete\{id}\variant', [VariantController::class, 'destroy'])->name('variant.delete');
 
 
 
 // Route đăng kí
-route::get('register',[AuthController::class,'formRegister'])->name("register");
-route::post('post-register',[AuthController::class,'postRegister'])->name("post-register");
+route::get('register', [AuthController::class, 'formRegister'])->name("register");
+route::post('post-register', [AuthController::class, 'postRegister'])->name("post-register");
+
 // Route đăng nhập
-route::get('login',[AuthController::class,'formLogin'])->name("login");
-route::post('post-login',[AuthController::class,'postLogin'])->name('post-login');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', function () {
+        return view('/');
+    });
+});
+route::get('login', [AuthController::class, 'formLogin'])->name("login");
+route::post('post-login', [AuthController::class, 'postLogin'])->name('post-login');
 // Route đăng xuất
-route::get('logout',[AuthController::class,'logout'])->name("logout");
-
-
+route::get('logout', [AuthController::class, 'logout'])->name("logout");
