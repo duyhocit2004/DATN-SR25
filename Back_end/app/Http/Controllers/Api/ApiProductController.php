fdf7614b090a31360bf7693fbb8e7cf669ffebd0
Nam2808-git <nam28082004@gmail.com>
2025-02-14 17:48:32 +0700

    'Capnhat'

--- Back_end/app/Http/Controllers/Api/ApiProductController.php
+++ Back_end/app/Http/Controllers/Api/ApiProductController.php
@@ -3,27 +3,46 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductRequest;
+use App\Http\Resources\ProductResource;
 use App\Models\products;
 use Illuminate\Http\Request;
 
 class ApiProductController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Danh sách sản phẩm
      */
     public function index()
     {
-        $products = products::query()->get();
-        return response()->json($products);
-        
+        $query = products::query()->get();
+
+        // return response()->json($products);
+
+        $products = $query->paginate(2);
+
+        return ProductResource::collection($products);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $params = $request->all();
+
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('uploads/products', 'public');
+            $params['image'] = $imagePath;
+        }
+
+        $products = Products::create($params);
+
+        return response()->json([
+            'data' => new ProductResource($products),
+            'success' => true,
+            'message' => 'Thêm thành công'
+        ], 201);
     }
 
     /**
