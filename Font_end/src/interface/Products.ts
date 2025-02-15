190f161c578cde285ce6b01e7e746b42d64ea49b
Chinhle2905 <lechinh2905@gmail.com>
2025-02-15 22:06:46 +0700

    cap nhat

---
+++ Font_end/src/interface/Products.ts
@@ -0,0 +1,11 @@
+export interface IProducts {
+    categories_id: number;
+    name_product: string;
+    SKU: string; 
+    base_stock: number;
+    price_regular: number;
+    price_sale: number;
+    description: string;
+    views: number;
+    content: string;
+}
---
+++ Font_end/src/sevice/productSevice.ts
@@ -0,0 +1,7 @@
+import axiosInstance from '../axios/config';
+import { IProducts } from '../interface/Products';
+
+export const getProducts = async (): Promise<IProducts[]> => {
+    const response = await axiosInstance.get<IProducts[]>('/products'); //thay theo API
+    return response.data;
+};
