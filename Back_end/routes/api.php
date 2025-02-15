739f112ad0b6736e7d62598d3ce2da31f778d2a9
duongtruong2 <truongdz2903@gmail.com>
2025-02-16 00:03:56 +0700

    api user

--- Back_end/routes/api.php
+++ Back_end/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\ApiProductController;
+use App\Http\Controllers\Api\ApiUserController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::get('products', [ApiProductController::class, 'index']);
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+// Route::get('products', [ApiProductController::class, 'index']);
+
+Route::get('users', [ApiUserController::class, 'index']);
+
+Route::post('users', [ApiUserController::class, 'store']);
+Route::get('users/{id}', [ApiUserController::class, 'show']);
+Route::put('users/{id}', [ApiUserController::class, 'update']);
+Route::delete('users/{id}', [ApiUserController::class, 'destroy']);
