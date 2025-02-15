3062e22d11a22c7c7e0ce9b34aafd70e0954a206
duyhocit <hoangduyoffline@gmail.com>
2025-02-15 23:08:16 +0700

    Database

--- Back_end/routes/api.php
+++ Back_end/routes/api.php
@@ -17,12 +17,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->group( function (){
-Route::get('/users', [ApiUserController::class, 'user']);
 Route::delete('/logout',[ApiAuthController::class,'logout']);
 Route::get('/user',[ApiAuthController::class,'user']);
 });
 
-Route::get('products', [ApiProductController::class, 'index']);
+// Route::get('products', [ApiProductController::class, 'index']);
 Route::post('login',[ApiAuthController::class,'login']);
 Route::post('register',[ApiAuthController::class,'register']);
 
