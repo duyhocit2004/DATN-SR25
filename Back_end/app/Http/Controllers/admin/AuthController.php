d9b36ff13abe4391cd3b0bb5422d110f45a14b1b
Nam2808-git <nam28082004@gmail.com>
2025-02-16 20:04:16 +0700

    Capnhat

--- Back_end/app/Http/Controllers/admin/AuthController.php
+++ Back_end/app/Http/Controllers/admin/AuthController.php
@@ -80,6 +80,6 @@ class AuthController extends Controller
     public function logout()
     {
         Auth::logout();
-        return redirect()->back();
+        return redirect('login');
     }
 }
