b3d6567a71d17cc5162d00b43529ebebc3df8948
duyhocit <hoangduyoffline@gmail.com>
2025-02-16 22:02:18 +0700

    cap nhat

--- Back_end/app/Http/Controllers/Api/ApiBannerController.php
+++ Back_end/app/Http/Controllers/Api/ApiBannerController.php
@@ -69,13 +69,8 @@ class ApiBannerController extends Controller
      */
     public function show(Request $request ,string $id)
     {
-        if($request->route('type')){
-            $type = $request->route('type');
-            Banners::query()->where('type' ,'=','$type');
-            return response()->json([
-                'data' => $type
-            ]);
-        }ifel
+        $Banner = Banners::query()->findOrFail($id);
+        
     }
 
     /**
