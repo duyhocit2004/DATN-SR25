6b1412be0a94910cc533b847d060dd062d7f14b8
nguyenghia <nghiajerry123@gmail.com>
2025-04-05 15:43:35 +0700

    update comment

--- Back_end/app/Models/Comment.php
+++ Back_end/app/Models/Comment.php
@@ -41,11 +41,28 @@ class Comment extends Model
 		'content',
 		'rate',
 		'phone_number'
+
 	];
 
 	public function product()
 	{
+		// Định nghĩa mối quan hệ với bảng Product
+		// Sử dụng belongsTo để chỉ định rằng mỗi comment thuộc về một sản phẩm
+		// 'product_id' là khóa ngoại trong bảng comment
 		return $this->belongsTo(Product::class);
+	}
+
+	public function parent()
+	{
+		return $this->belongsTo(Comment::class, 'parent_id');  // 'parent_id' là cột tham chiếu đến id của category cha		
+		// Sử dụng belongsTo để chỉ định rằng mỗi comment có thể có một comment cha
+		// 'parent_id' là khóa ngoại trong bảng comment
+		// Nếu comment không có comment cha, thì parent_id sẽ là null
+		// Nếu comment có comment cha, thì parent_id sẽ chứa id của comment cha
+		// Điều này cho phép chúng ta truy cập thông tin của comment cha từ comment hiện tại
+
+
+		// Ví dụ: $comment->parent sẽ trả về comment cha của comment hiện tại
 	}
 
     public function children()
